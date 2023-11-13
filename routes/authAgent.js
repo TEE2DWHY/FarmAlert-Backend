8ca65a90b68e50bc70c74ab73b67ce3bb6cb4000
TEE2DWHY <isaacolorunfemi330@gmail.com>
2023-11-13 08:14:04 +0100

    modified authAgent Router

--- routes/authAgent.js
+++ routes/authAgent.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { register, login, verifyEmail } = require("../controllers/authAgent");
 
 router.post("/agent/register", register);
-router.get("/agent/verify-email", verifyEmail);
+router.post("/agent/verify-email", verifyEmail);
 router.post("/agent/login", login);
 
 module.exports = router;
--- routes/authUser.js
+++ routes/authUser.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { register, login } = require("../controllers/authUser");
 
 router.post("/user/register", register);
-// router.get("/user/verify-email", verifyEmail);
+// router.post("/user/verify-email", verifyEmail);
 router.post("/user/login", login);
 
 module.exports = router;
