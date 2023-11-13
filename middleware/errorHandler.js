dfe82e5063c0bfcdf097cd8c78d5ec72e6abc0ca
TEE2DWHY <isaacolorunfemi330@gmail.com>
2023-11-13 22:27:29 +0100

    changed agent and user router middleware structure

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,15 +1,24 @@
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandler = async (err, req, res, next) => {
+  if (err.name === "ValidationError") {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      msg: err.message,
+    });
+  }
   if (err.code === 11000) {
     return res.status(StatusCodes.BAD_REQUEST).json({
-      msg: `${Object.keys(err.keyValue)} is taken already`,
+      msg: `${Object.keys(err.keyValue)} is taken already.`,
+    });
+  }
+  if (err.name === "CastError") {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      msg: `${err.value} is not found in database.`,
     });
   }
   res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
     msg: err.message,
   });
-  next();
 };
 
 module.exports = errorHandler;
