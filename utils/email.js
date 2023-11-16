409ce66e1f503ebdd4dbffd13691c93c4a9f1b52
TEE2DWHY <isaacolorunfemi330@gmail.com>
2023-11-16 16:40:20 +0100

    modified email.js

--- utils/email.js
+++ utils/email.js
@@ -55,7 +55,7 @@ const verifyEmailMessage = (verificationToken, currentUser, name) => {
           <p>We are super excited to have you here. Trust me, we waited a long time to have you on board, and here you are.
           It's impressive that you've taken such a bold step to become a better version of yourself, we are glad to help you walk through this journey.
           <br/> <br/>
-          <span class="link">Kindly click this <a href=${process.env.BASE_URL}/auth/${currentUser}/verify-email?token=${verificationToken}>link</a> to verify your account.</span>
+          <span class="link">Kindly click this <a href=${process.env.BASE_URL}/${currentUser}-verification?token=${verificationToken}>link</a> to verify your account.</span>
           <br/>
           Stay tuned for insightful newsletter and other pertinent updates.
           <br/> <br/>
