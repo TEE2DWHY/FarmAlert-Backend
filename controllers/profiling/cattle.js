a76657415c6c1e48b9eec3d5ac29090f6725f0aa
TEE2DWHY <isaacolorunfemi330@gmail.com>
2023-11-21 20:43:01 +0100

    added get functions to cattle controller

--- controllers/profiling/cattle.js
+++ controllers/profiling/cattle.js
@@ -3,6 +3,7 @@ const asyncWrapper = require("../../middleware/asyncWrapper");
 const Cattle = require("../../models/Cattle");
 const cloudinary = require("../../utils/cloudinary");
 const moment = require("moment");
+const jwt = require("jsonwebtoken");
 
 const register = asyncWrapper(async (req, res) => {
   let result;
@@ -28,9 +29,11 @@ const register = asyncWrapper(async (req, res) => {
       vaccinationDate: parsedVaccineDate,
       dateOfTreatment: parsedDateOfTreatment,
     });
+    const token = jwt.sign({ cattleId: cattle.Id });
     res.status(StatusCodes.CREATED).json({
       message: "New Cattle Profile Added.",
       cattle: cattle,
+      token: token,
     });
   } catch (err) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
@@ -42,4 +45,32 @@ const register = asyncWrapper(async (req, res) => {
   }
 });
 
-module.exports = register;
+// Get All Cattle
+const allCattle = asyncWrapper(async (req, res) => {
+  const cattle = await Cattle.find();
+  res.status(StatusCodes.OK).json({
+    message: {
+      allCattle: cattle,
+    },
+  });
+});
+
+// Get A Specific Cattle
+const getCattle = asyncWrapper(async (req, res) => {
+  const { token } = req.query;
+  if (!token) {
+    res.status(StatusCodes.OK).json({
+      message: "Please Provide Token.",
+    });
+  }
+  const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+  const { cattleId } = decodedToken;
+  const cattle = await Cattle.find({ Id: cattleId });
+  res.status(StatusCodes.OK).json({
+    message: {
+      cattle: cattle,
+    },
+  });
+});
+
+module.exports = { register, allCattle, getCattle };
