06073e52a1142fe093b2430cab10a5dff7f8fda6
TEE2DWHY <isaacolorunfemi330@gmail.com>
2024-05-22 00:02:59 +0100

    modified farm alert welcome email

--- utils/email.js
+++ utils/email.js
@@ -52,13 +52,15 @@ const verifyEmailMessage = (verificationToken, currentUser, name) => {
         <div class="container">
           <h1 class="welcome">Welcome on Board.</h1>
           <p>Hi ${name},</p>
-          <p>We are super excited to have you here. Trust me, we waited a long time to have you on board, and here you are.
-          It's impressive that you've taken such a bold step to become a better version of yourself, we are glad to help you walk through this journey.
+          <p>Welcome to Farm Alert Animal Identification and Management Application, where you can easily identify, track, and manage your animals.
           <br/> <br/>
           <span class="link">Kindly click this <a href=${process.env.BASE_URL}/${currentUser}-verification?token=${verificationToken}>link</a> to verify your account.</span>
           <br/>
-          Stay tuned for insightful newsletter and other pertinent updates.
+          We are excited to have you on board!.
           <br/> <br/>
+          <p>For support, email us at: 
+          <a href="mailto:support@farmalert.com.ng">support@farmalert.com.ng</a>
+          </p>
           </p>
         </div>
       </body>
