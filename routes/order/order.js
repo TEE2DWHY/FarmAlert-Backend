d9c9cb96fdcdb4712c912cb84280681779da1040
TEE2DWHY <isaacolorunfemi330@gmail.com>
2024-07-02 06:46:36 +0100

    added the create order endpoint for the order.js controller

--- routes/order/order.js
+++ routes/order/order.js
@@ -10,7 +10,7 @@ const {
 router.get("/get-order/:id", getOrder);
 router.get("/get-order", getAllOrders);
 router.post("/create-order/:productId", createOrder);
-router.get("/transaction-status/:reference", getOrderStatus);
+router.get("/transaction-status/:orderId", getOrderStatus);
 // router.patch("/update-transaction-status", updateTransactionStatus);
 
 module.exports = router;
